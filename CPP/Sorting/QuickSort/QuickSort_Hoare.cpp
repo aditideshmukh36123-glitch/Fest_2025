bedcb2f88384918cfa8afe2920a4b3fca8929ef3
Anurag Singh <singhanurag1309@gmail.com>
2025-10-03 22:05:14 +0530

    added QuickSort Hoare algorithm

---
+++ CPP/Sorting/QuickSort/QuickSort_Hoare.cpp
@@ -0,0 +1,47 @@
+// QuickSort_Hoare.cpp
+// Hoare Partition Scheme for Quick Sort
+
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low - 1;
+    int j = high + 1;
+
+    while (true) {
+        do {
+            i++;
+        } while (arr[i] < pivot);
+
+        do {
+            j--;
+        } while (arr[j] > pivot);
+
+        if (i >= j)
+            return j;
+
+        swap(arr[i], arr[j]);
+    }
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int arr[] = {24, 9, 29, 14, 19, 27};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
